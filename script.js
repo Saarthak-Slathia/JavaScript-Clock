a48d4fa429071521d165ba2edc088fc867142d45
Saarthak Slathia <77338782+Saarthak-Slathia@users.noreply.github.com>
2021-01-25 10:20:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // Importing the h1 tag.
 let date = document.getElementById('time');
 
-// Defining and updating the time.
+// Defining and updating the time by every 1 second.
 setInterval(() => {
     let newtime = new Date();
     let time = new Date().toLocaleTimeString();
-    date.innerText = `Current time is ${time}`;
+    date.innerText = `Current time is ${time}`; // Displaying the time on the screen.
 }, 1000);
 
-// Our JavaScript Clock is ready !!! 
+// Our JavaScript part is completed, you can style it further using CSS.
 
+// Our JavaScript Clock is ready !!! 
 
